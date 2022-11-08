b6ef542dbaf07c85ea86facfacc67b4e1103e7a1
DG <gdavidcefim@gmail.com>
2022-11-08 09:17:46 +0100

    Multiple terms search func

--- src/lib/func/search.ts
+++ src/lib/func/search.ts
@@ -1,17 +1,28 @@
 export const searchingData = (term: string, data: Object[]) => {
-    if (term === '' || term.split('').shift() === ' ') {
+    const searchedTerms = term.split(' ');
+    let results = data;
+
+    if (term.split('').shift() === ' ') {
         return data;
     }
 
+    /**
+     * Convert values in object into string
+     * @param object
+     * @returns
+     */
     const objectToStringLowerCase = (object: Object) => {
+        console.log('str', JSON.stringify(object).toLowerCase());
         return JSON.stringify(object).toLowerCase();
     };
 
-    const termToLowerCase = term.toLowerCase();
+    for (const elt of searchedTerms) {
+        results = results.filter((employee) =>
+            objectToStringLowerCase(Object.values(employee)).includes(
+                elt.toLowerCase()
+            )
+        );
+    }
 
-    return data.filter((employee) =>
-        objectToStringLowerCase(Object.values(employee)).includes(
-            termToLowerCase
-        )
-    );
+    return results;
 };
